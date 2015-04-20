086f540819f37fd44f78a3cefaa08b90f72339bc
Angela Banuelos <banuelosangela56@gmail.com>
2015-04-20 12:00:53 -0700

    Assessment

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('div').click(function(){
+	alert("Hey you ckicked me!")
+	});
+});
+
+$(document).ready(function(){
+	$('#iwao').click(function(){
+	alert("hey you clicked me again!");
+
+	});	
+});
